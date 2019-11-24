aeeca92fdaa188502145f677511bb50c87fc953d
Luis Carlos Figueroa Rodríguez <lc.figueroar26@gmail.com>
2019-11-24 11:22:12 -0600

    Regions model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_171842) do
+ActiveRecord::Schema.define(version: 2019_11_24_172114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 2019_11_24_171842) do
   create_table "days", force: :cascade do |t|
     t.string "name"
     t.integer "number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "regions", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
